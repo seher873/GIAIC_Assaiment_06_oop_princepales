57a6dbf6be235d0526b858906c70c94ac89ca64c
Seher khan <161340426+seher873@users.noreply.github.com>
2025-06-28 21:11:51 +0500

    Add files via upload

---
+++ 00_intro_python/add_two_num.py
@@ -0,0 +1,32 @@
+#starter code
+def main():
+    print("delete this line and write your code!:")
+    #this provder line  is requierd at the end of python file
+   # to call the main() function
+if __name__ =='__main__':
+  main()
+
+
+#Sulution
+
+"""Program add2numbers
+_____________
+Another python program to get some practice with
+variables.  This program asks the user for two
+integers and prints their sum.
+"""
+def main():
+    print("This program adds two numbers.")
+    num1:str=input("Enter first number: ")
+    num1:int=int(num1)
+    num2:str=input("Enter second number: ")
+    num2:int=int(num2)
+    total:int=num1+num2
+    print("the total is " + str(total) + ".")
+
+                               
+
+ #this provder line  is requierd at the end of python file
+   # to call the main() function
+if __name__ =='__main__':
+  main()
---
+++ 00_intro_python/agreement_bot.py
@@ -0,0 +1,8 @@
+#Solution
+def main():
+    favorite_animal = input("What's your favorite animal? ")
+    print(f"My favorite animal is also {favorite_animal}!")
+
+if __name__ == '__main__':
+    main()
+# This code is a simple agreement bot that asks the user for their favorite animal and responds with the same animal.
